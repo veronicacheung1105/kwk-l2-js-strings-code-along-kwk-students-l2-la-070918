13e1717cc661017c0ec3b572c88c1a6ae7cc77f0
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-11 20:32:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your code in this file!
-
+const currentUser = 'Koders';
+const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
