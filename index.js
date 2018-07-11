100889745ed98da2b0068fe93296e518faa97c70
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-11 20:31:16 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,9 +0,0 @@
-// Write your code in this file!
-
-const currentUser = 'Grace Hopper'
-
-const welcomeMessage = 'Welcome to Klossybook, ' + currentUser + '!' ;
-
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser}`;
